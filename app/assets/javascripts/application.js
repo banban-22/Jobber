84d6983bfd1c6590142f52024b7c99ea17422795
AyakaB <ayaka.bando22@gmail.com>
2023-06-01 16:07:16 -0700

    fixed index design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,6 @@
 //= require bootstrap
 //= require rails-ujs
 //= require jquery
-//= require jquery-ui/widgets/autocomplete
 //= require tags-autocomplete
 //= require_tree .
 //= require chartkick
